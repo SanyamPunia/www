b8994cc7ea029bc02d2444a1e270c233980c52e1
SanyamPunia <lewarends@gmail.com>
2024-09-25 22:05:06 +0530

    feat: :bulb: update project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,6 +8,30 @@ const Projects = (props: Props) => {
     <div className="mt-16 sm:-mx-2 text-xs">
       <h1 className="text-xs text-muted-foreground mb-3">/projects</h1>
 
+      <a href="https://github.com/SanyamPunia/profanity-api" target="_blank">
+        <div className="flex gap-4 transition-all hover:bg-secondary/40 rounded-xl px-4 -mx-4 pt-3">
+          <div className="bg-neutral-100 dark:bg-neutral-800 w-[36px] h-[36px] rounded-full mt-[2px] flex-shrink-0 shadow-shorter overflow-hidden">
+            <div className="text-sm text-neutral-400 font-semibold flex justify-center items-center h-full">
+              <Image
+                src="/profanity.jpg"
+                width={36}
+                height={36}
+                alt="profanity-logo"
+                draggable="false"
+                className="select-none"
+              />
+            </div>
+          </div>
+          <div className="w-full pb-4 border-b border-secondary/40">
+            <h1 className="font-medium">Profanity API</h1>
+            <div className="mt-px lowercase text-muted-foreground flex justify-between gap-2 items-center">
+              <p>Profanity check at scale using hono, upstash & cloudflare</p>
+              <span className="px-2 py-px rounded-lg bg-secondary">api</span>
+            </div>
+          </div>
+        </div>
+      </a>
+
       <a href="https://skillup-ncu.vercel.app/" target="_blank">
         <div className="flex gap-4 transition-all hover:bg-secondary/40 rounded-xl px-4 -mx-4 pt-3">
           <div className="bg-neutral-100 dark:bg-neutral-800 w-[36px] h-[36px] rounded-[10px] mt-[2px] flex-shrink-0 shadow-shorter overflow-hidden">
@@ -46,7 +70,14 @@ const Projects = (props: Props) => {
         <div className="flex gap-4 transition-all hover:bg-secondary/40 rounded-xl px-4 -mx-4 pt-3">
           <div className="bg-neutral-100 dark:bg-neutral-800 w-[36px] h-[36px] rounded-full mt-[2px] flex-shrink-0 shadow-shorter overflow-hidden">
             <div className="text-sm text-neutral-400 font-semibold flex justify-center items-center h-full">
-              <Image src="/flib.png" width={36} height={36} alt="flib-logo" />
+              <Image
+                src="/flib.png"
+                width={36}
+                height={36}
+                alt="flib-logo"
+                draggable="false"
+                className="select-none"
+              />
             </div>
           </div>
           <div className="w-full pb-4 border-b border-secondary/40">
