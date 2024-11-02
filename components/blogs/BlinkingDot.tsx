f2916a3f4d70e726dad29b97174a3b610b794816
SanyamPunia <lewarends@gmail.com>
2024-11-03 01:45:52 +0530

    remove mt

--- components/blogs/BlinkingDot.tsx
+++ components/blogs/BlinkingDot.tsx
@@ -1,6 +1,6 @@
 export const BlinkingDot = () => {
   return (
-    <div className="bg-border/20 relative rounded-md py-4 px-4 flex items-center text-neutral-400 mt-8 mb-4">
+    <div className="bg-border/20 relative rounded-md py-4 px-4 flex items-center text-neutral-400 mb-4">
       <div className="rounded-full bg-green-400 h-[8px] w-[8px] inline-block mr-2"></div>
       <div className="absolute animate-ping rounded-full bg-green-400 h-[8px] w-[8px] mr-2 "></div>
 
