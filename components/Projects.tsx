28f47e4a71e1c6369d38bfc10fe88a97c81d9bd1
SanyamPunia <lewarends@gmail.com>
2024-12-05 19:23:45 +0530

    fix order

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,25 +8,25 @@ const Projects = (props: Props) => {
     <div className="mt-16 sm:-mx-2 text-xs">
       <h1 className="text-xs text-muted-foreground mb-3">/projects</h1>
 
-      <a href="https://on-snip.org" target="_blank">
+      <a href="https://github.com/SanyamPunia/profanity-api" target="_blank">
         <div className="flex gap-4 transition-all hover:bg-secondary/40 rounded-xl px-4 -mx-4 pt-3">
           <div className="bg-neutral-100 dark:bg-neutral-800 w-[36px] h-[36px] rounded-full mt-[2px] flex-shrink-0 shadow-shorter overflow-hidden">
             <div className="text-sm text-neutral-400 font-semibold flex justify-center items-center h-full">
               <Image
-                src="/projects/onsnip.png"
+                src="/projects/profanity.jpg"
                 width={36}
                 height={36}
-                alt="on-snip-logo"
+                alt="profanity-logo"
                 draggable="false"
                 className="select-none"
               />
             </div>
           </div>
           <div className="w-full pb-4 border-b border-secondary/40">
-            <h1 className="font-medium">on-snip.org</h1>
+            <h1 className="font-medium">Profanity API</h1>
             <div className="mt-px lowercase text-muted-foreground flex justify-between gap-2 items-center">
-              <p>Real-time Collaborative Messaging Rooms</p>
-              <span className="px-2 py-px rounded-lg bg-secondary">web</span>
+              <p>Profanity check at scale using hono, upstash & cloudflare</p>
+              <span className="px-2 py-px rounded-lg bg-secondary">api</span>
             </div>
           </div>
         </div>
@@ -84,25 +84,25 @@ const Projects = (props: Props) => {
         </div>
       </a>
 
-      <a href="https://github.com/SanyamPunia/profanity-api" target="_blank">
+      <a href="https://on-snip.org" target="_blank">
         <div className="flex gap-4 transition-all hover:bg-secondary/40 rounded-xl px-4 -mx-4 pt-3">
           <div className="bg-neutral-100 dark:bg-neutral-800 w-[36px] h-[36px] rounded-full mt-[2px] flex-shrink-0 shadow-shorter overflow-hidden">
             <div className="text-sm text-neutral-400 font-semibold flex justify-center items-center h-full">
               <Image
-                src="/projects/profanity.jpg"
+                src="/projects/onsnip.png"
                 width={36}
                 height={36}
-                alt="profanity-logo"
+                alt="on-snip-logo"
                 draggable="false"
                 className="select-none"
               />
             </div>
           </div>
           <div className="w-full pb-4 border-b border-secondary/40">
-            <h1 className="font-medium">Profanity API</h1>
+            <h1 className="font-medium">on-snip.org</h1>
             <div className="mt-px lowercase text-muted-foreground flex justify-between gap-2 items-center">
-              <p>Profanity check at scale using hono, upstash & cloudflare</p>
-              <span className="px-2 py-px rounded-lg bg-secondary">api</span>
+              <p>Real-time Collaborative Messaging Rooms</p>
+              <span className="px-2 py-px rounded-lg bg-secondary">web</span>
             </div>
           </div>
         </div>
