5ebc2dc017d0028ef86fac5552bb309dc335e2e2
SanyamPunia <lewarends@gmail.com>
2025-01-05 17:42:27 +0530

    chore: :lipstick: add project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -132,6 +132,30 @@ const Projects = (props: Props) => {
         </div>
       </a>
 
+      <a href="https://better-gist.vercel.app/" target="_blank">
+        <div className="flex gap-4 transition-all hover:bg-secondary/40 rounded-xl px-4 -mx-4 pt-3">
+          <div className="bg-neutral-100 dark:bg-neutral-800 w-[36px] h-[36px] rounded-full mt-[2px] flex-shrink-0 shadow-shorter overflow-hidden">
+            <div className="text-sm text-neutral-400 font-semibold flex justify-center items-center h-full">
+              <Image
+                src="/projects/bg.png"
+                width={36}
+                height={36}
+                alt="better-gist-logo"
+                draggable="false"
+                className="select-none"
+              />
+            </div>
+          </div>
+          <div className="w-full pb-4 border-b border-secondary/40">
+            <h1 className="font-medium">better-gist</h1>
+            <div className="mt-px lowercase text-muted-foreground flex justify-between gap-2 items-center">
+              <p>generate `shareable` code snippets.</p>
+              <span className="px-2 py-px rounded-lg bg-secondary">web</span>
+            </div>
+          </div>
+        </div>
+      </a>
+
       <a href="https://stick-it-olive.vercel.app/" target="_blank">
         <div className="flex gap-4 transition-all hover:bg-secondary/40 rounded-xl px-4 -mx-4 pt-3">
           <div className="bg-neutral-100 dark:bg-neutral-800 w-[36px] h-[36px] rounded-full mt-[2px] flex-shrink-0 shadow-shorter overflow-hidden">
@@ -175,30 +199,6 @@ const Projects = (props: Props) => {
             <div className="mt-px lowercase text-muted-foreground flex justify-between gap-2 items-center">
               <p>Quick Q&A for university students.</p>
 
-              <span className="px-2 py-px rounded-lg bg-secondary">web</span>
-            </div>
-          </div>
-        </div>
-      </a>
-
-      <a href="https://6ed9fd44.gym-landing-page.pages.dev/" target="_blank">
-        <div className="flex gap-4 transition-all hover:bg-secondary/40 rounded-xl px-4 -mx-4 pt-3">
-          <div className="bg-neutral-100 dark:bg-neutral-800 w-[36px] h-[36px] rounded-full mt-[2px] flex-shrink-0 shadow-shorter overflow-hidden">
-            <div className="text-sm text-neutral-400 font-semibold flex justify-center items-center h-full">
-              <Image
-                src="/projects/evogym.png"
-                width={36}
-                height={36}
-                alt="evogym-logo"
-                draggable="false"
-                className="select-none"
-              />
-            </div>
-          </div>
-          <div className="w-full pb-4 border-b border-secondary/40">
-            <h1 className="font-medium">EvoGym</h1>
-            <div className="mt-px lowercase text-muted-foreground flex justify-between gap-2 items-center">
-              <p>Responsive GYM page using VITE, Framer, Cloudflare.</p>
               <span className="px-2 py-px rounded-lg bg-secondary">web</span>
             </div>
           </div>
